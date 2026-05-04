8bebed17b08a71557884959148930d3c41412030
Владимир Воронков <temlord34@gmail.com>
2026-05-04 13:36:02 +0500

    Задание 3.6

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,74 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            buttonReset = new Button();
-            buttonAlarm = new Button();
-            pnlWindow = new Panel();
-            pnlDoor = new Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            buttonAnswer1 = new Button();
+            label1 = new Label();
+            textBoxX = new TextBox();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBoxN = new TextBox();
+            textBoxAnswer1 = new TextBox();
+            label5 = new Label();
+            pictureBox2 = new PictureBox();
+            buttonAnswer2 = new Button();
+            textBoxAnswer2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
-            // buttonReset
+            // buttonAnswer1
             // 
-            buttonReset.Location = new Point(680, 20);
-            buttonReset.Name = "buttonReset";
-            buttonReset.Size = new Size(100, 40);
-            buttonReset.TabIndex = 6;
-            buttonReset.Text = "Сброс";
-            buttonReset.UseVisualStyleBackColor = true;
-            buttonReset.Click += buttonReset_Click;
+            buttonAnswer1.Location = new Point(36, 192);
+            buttonAnswer1.Name = "buttonAnswer1";
+            buttonAnswer1.Size = new Size(176, 38);
+            buttonAnswer1.TabIndex = 0;
+            buttonAnswer1.Text = "Вычислить ответ";
+            buttonAnswer1.UseVisualStyleBackColor = true;
+            buttonAnswer1.Click += buttonAnswer1_Click;
             // 
-            // buttonAlarm
+            // label1
             // 
-            buttonAlarm.Location = new Point(550, 20);
-            buttonAlarm.Name = "buttonAlarm";
-            buttonAlarm.Size = new Size(120, 40);
-            buttonAlarm.TabIndex = 4;
-            buttonAlarm.Text = "Сигнализация";
-            buttonAlarm.UseVisualStyleBackColor = true;
-            buttonAlarm.Click += buttonAlarm_Click;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Вычислить:";
             // 
-            // pnlWindow
+            // textBoxX
             // 
-            pnlWindow.BackColor = Color.LightSkyBlue;
-            pnlWindow.Location = new Point(275, 360);
-            pnlWindow.Name = "pnlWindow";
-            pnlWindow.Size = new Size(90, 90);
-            pnlWindow.TabIndex = 0;
-            pnlWindow.Paint += pnlWindow_Paint;
-            pnlWindow.Click += SecurityZone_Click;
-            pnlWindow.Click += Window_Click;
+            textBoxX.Location = new Point(63, 136);
+            textBoxX.Name = "textBoxX";
+            textBoxX.Size = new Size(125, 27);
+            textBoxX.TabIndex = 2;
             // 
-            // pnlDoor
+            // pictureBox1
             // 
-            pnlDoor.BackColor = Color.SaddleBrown;
-            pnlDoor.Location = new Point(435, 410);
-            pnlDoor.Name = "pnlDoor";
-            pnlDoor.Size = new Size(70, 110);
-            pnlDoor.TabIndex = 1;
-            pnlDoor.Paint += pnlDoor_Paint;
-            pnlDoor.Click += SecurityZone_Click;
-            pnlDoor.Click += Door_Click;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(53, 40);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(282, 49);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(103, 28);
+            label2.TabIndex = 4;
+            label2.Text = "Введите:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 28);
+            label3.TabIndex = 5;
+            label3.Text = "Х =";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label4.Location = new Point(230, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 28);
+            label4.TabIndex = 6;
+            label4.Text = "N =";
+            // 
+            // textBoxN
+            // 
+            textBoxN.Location = new Point(280, 136);
+            textBoxN.Name = "textBoxN";
+            textBoxN.Size = new Size(125, 27);
+            textBoxN.TabIndex = 7;
+            // 
+            // textBoxAnswer1
+            // 
+            textBoxAnswer1.Location = new Point(280, 198);
+            textBoxAnswer1.Name = "textBoxAnswer1";
+            textBoxAnswer1.ReadOnly = true;
+            textBoxAnswer1.Size = new Size(228, 27);
+            textBoxAnswer1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label5.Location = new Point(12, 268);
+            label5.Name = "label5";
+            label5.Size = new Size(132, 28);
+            label5.TabIndex = 9;
+            label5.Text = "Вычислить:";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.InitialImage = null;
+            pictureBox2.Location = new Point(109, 314);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(226, 74);
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            // 
+            // buttonAnswer2
+            // 
+            buttonAnswer2.Location = new Point(36, 424);
+            buttonAnswer2.Name = "buttonAnswer2";
+            buttonAnswer2.Size = new Size(176, 38);
+            buttonAnswer2.TabIndex = 11;
+            buttonAnswer2.Text = "Вычислить ответ";
+            buttonAnswer2.UseVisualStyleBackColor = true;
+            buttonAnswer2.Click += buttonAnswer2_Click;
+            // 
+            // textBoxAnswer2
+            // 
+            textBoxAnswer2.Location = new Point(280, 430);
+            textBoxAnswer2.Name = "textBoxAnswer2";
+            textBoxAnswer2.ReadOnly = true;
+            textBoxAnswer2.Size = new Size(228, 27);
+            textBoxAnswer2.TabIndex = 12;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(782, 553);
-            Controls.Add(pnlWindow);
-            Controls.Add(pnlDoor);
-            Controls.Add(buttonReset);
-            Controls.Add(buttonAlarm);
+            BackColor = Color.FromArgb(153, 180, 209);
+            ClientSize = new Size(582, 553);
+            Controls.Add(textBoxAnswer2);
+            Controls.Add(buttonAnswer2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label5);
+            Controls.Add(textBoxAnswer1);
+            Controls.Add(textBoxN);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBoxX);
+            Controls.Add(label1);
+            Controls.Add(buttonAnswer1);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
-            Text = "Умный(не особо) Дом";
+            Text = "What is this diddy blud doin' on the calculator";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button buttonReset;
-        private Button buttonAlarm;
-        private Panel pnlWindow;
-        private Panel pnlDoor;
+        private Button buttonAnswer1;
+        private Label label1;
+        private TextBox textBoxX;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxN;
+        private TextBox textBoxAnswer1;
+        private Label label5;
+        private PictureBox pictureBox2;
+        private Button buttonAnswer2;
+        private TextBox textBoxAnswer2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -8,167 +8,64 @@ namespace WinFormsAppEdu
 {
     public partial class Form1 : Form
     {
-        private bool isLightsOn = false;
-        private bool isDoorOpen = false;
-        private bool isTriggered = false;
-        private bool isAlarmed = false;
-        private bool tickState = false;
-        private Timer alarmTimer;
 
         public Form1()
         {
             InitializeComponent();
-            this.DoubleBuffered = true;
-            this.Paint += Form1_Paint;
-            alarmTimer = new Timer();
-            alarmTimer.Interval = 400;
-            alarmTimer.Tick += AlarmTimer_Tick;
         }
 
-        private void Form1_Paint(object? sender, PaintEventArgs e)
+        private double Factorial(int n)
         {
-            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-
-            int houseX = 225;
-            int houseY = 300;
-            int houseWidth = 350;
-            int houseHeight = 220;
-
-            //Основная стена
-            using (var wallBrush = new SolidBrush(Color.LightGoldenrodYellow))
-            {
-                e.Graphics.FillRectangle(wallBrush, houseX, houseY, houseWidth, houseHeight);
-            }
-
-            int roofLeft = houseX - 20;
-            int roofRight = houseX + houseWidth + 20;
-            int roofTop = houseY;
-            int roofPeak = houseY - 120;
-            int roofPeakX = houseX + houseWidth / 2;
-
-            // Крыша (через путь)
-            using (var roofBrush = new SolidBrush(Color.Firebrick))
-            {
-                using (var path = new GraphicsPath())
-                {
-                    path.AddLine(roofLeft, roofTop, roofPeakX, roofPeak);
-                    path.AddLine(roofPeakX, roofPeak, roofRight, roofTop);
-                    path.AddLine(roofRight, roofTop, roofLeft, roofTop);
-                    e.Graphics.FillPath(roofBrush, path);
-                }
-            }
-
-            // Дымоход
-            using (SolidBrush chimneyBrush = new SolidBrush(Color.SaddleBrown))
-            {
-                e.Graphics.FillRectangle(chimneyBrush, houseX + 220, houseY - 130, 30, 90);
-            }
-
-            // Сигнализация
-            int alarmSize = 20;
-            int alarmX = pnlDoor.Location.X + (pnlDoor.Width / 2) - (alarmSize / 2);
-            int alarmY = pnlDoor.Location.Y - 30;
-            Color alarmColor;
-
-            if (isTriggered)
-            {
-                alarmColor = tickState ? Color.Red : Color.OrangeRed;
-            }
-            else
-            {
-                alarmColor = isAlarmed ? Color.DarkRed : Color.Gray;
-            }
-
-            using (SolidBrush alarmBrush = new SolidBrush(alarmColor))
-            {
-                e.Graphics.FillEllipse(alarmBrush, alarmX, alarmY, alarmSize, alarmSize);
-            }
+            double res = 1;
+            for (int i = 1; i <= n; i++) res *= i;
+            return res;
         }
 
-        // Крестовина Окна
-        private void pnlWindow_Paint(object? sender, PaintEventArgs e)
+        private void buttonAnswer1_Click(object sender, EventArgs e)
         {
-            using (Pen pen = new Pen(Color.Black, 2))
+            if (!double.TryParse(textBoxX.Text, out double x) || !int.TryParse(textBoxN.Text, out int n))
             {
-                int w = pnlWindow.Width;
-                int h = pnlWindow.Height;
-
-                e.Graphics.DrawLine(pen, 0, h / 2, w, h / 2);
-                e.Graphics.DrawLine(pen, w / 2, 0, w / 2, h);
-                e.Graphics.DrawRectangle(pen, 0, 0, w - 1, h - 1);
+                textBoxAnswer1.Text = "Ошибка ввода!";
+                return;
             }
-        }
-
-        // Ручка Двери
-        private void pnlDoor_Paint(object? sender, PaintEventArgs e)
-        {
-            Color handleColor = isDoorOpen ? pnlDoor.BackColor : Color.Gold;
-            using (SolidBrush handleBrush = new SolidBrush(handleColor))
+            if (n <= 0)
             {
-                int handleSize = 8;
-                e.Graphics.FillEllipse(handleBrush, pnlDoor.Width - 15, pnlDoor.Height / 2, handleSize, handleSize);
+                textBoxAnswer1.Text = "N должно быть > 0";
+                return;
             }
-        }
-
-        private void SecurityZone_Click(object sender, EventArgs e)
-        {
-            if (isAlarmed)
+            if (n > 100000)
             {
-                isTriggered = true;
-                alarmTimer.Start();
+                textBoxAnswer1.Text = "N слишком велико";
+                return;
             }
-        }
-
-        private void Window_Click(object sender, EventArgs e)
-        {
-            if (!isAlarmed)
+            if (Math.Abs(x) > 100)
             {
-                pnlWindow.BackColor = isLightsOn ? Color.Yellow : Color.LightSkyBlue;
-                isLightsOn = !isLightsOn;
-                this.Invalidate();
+                textBoxAnswer1.Text = "X вне диапазона";
+                return;
             }
-        }
 
-        private void Door_Click(object sender, EventArgs e)
-        {
-            if (!isAlarmed)
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
             {
-                isDoorOpen = !isDoorOpen;
-                pnlDoor.BackColor = isDoorOpen ? Color.DarkGray : Color.SaddleBrown;
-                this.Invalidate();
+                double power = Math.Pow(x, i);
+
+                if (double.IsInfinity(power)) break;
+
+                sum += Math.Sin(power);
             }
+            textBoxAnswer1.Text = sum.ToString("F6");
         }
 
-        private void buttonAlarm_Click(object sender, EventArgs e)
+        private void buttonAnswer2_Click(object sender, EventArgs e)
         {
-            isAlarmed = !isAlarmed;
-            isDoorOpen = false;
-            pnlDoor.BackColor = Color.SaddleBrown;
-
-            if (!isAlarmed)
+            double totalSum = 0;
+            for (int n = 1; n <= 10; n++)
             {
-                isTriggered = false;
-                alarmTimer.Stop();
+                double numerator = Math.Pow(Factorial(n), 2);
+                double denominator = Factorial(2 * n);
+                totalSum += numerator / denominator;
             }
-            this.Invalidate();
-        }
-
-        private void buttonReset_Click(object sender, EventArgs e)
-        {
-            isAlarmed = false;
-            isTriggered = false;
-            isLightsOn = false;
-            isDoorOpen = false;
-            pnlWindow.BackColor = Color.LightSkyBlue;
-            pnlDoor.BackColor = Color.SaddleBrown;
-            alarmTimer.Stop();
-            this.Invalidate();
-        }
-
-        private void AlarmTimer_Tick(object? sender, EventArgs e)
-        {
-            tickState = !tickState;
-            this.Invalidate();
+            textBoxAnswer2.Text = totalSum.ToString("F10");
         }
     }
 }
