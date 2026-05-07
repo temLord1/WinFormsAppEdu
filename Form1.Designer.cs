48add663400399dd86773e9c23031c35ea390be8
Владимир Воронков <temlord34@gmail.com>
2026-05-07 13:06:30 +0500

    Задание 4.1 - фикс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,227 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            labelSurname = new Label();
-            textBoxSurname = new TextBox();
-            dateTimePicker1 = new DateTimePicker();
-            comboBoxFlowers = new ComboBox();
-            panel1 = new Panel();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            comboBoxColors = new ComboBox();
-            label5 = new Label();
-            textBoxFlowers = new TextBox();
-            label6 = new Label();
-            textBoxLetter = new TextBox();
-            label7 = new Label();
-            textBoxSummary = new TextBox();
-            textBoxName = new TextBox();
-            labelName = new Label();
-            textBoxFathname = new TextBox();
-            labelFathname = new Label();
-            labelErr1 = new Label();
-            labelErr2 = new Label();
-            labelErr3 = new Label();
-            labelErr4 = new Label();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
-            // labelSurname
+            // listBox1
             // 
-            labelSurname.AutoSize = true;
-            labelSurname.Location = new Point(34, 14);
-            labelSurname.Name = "labelSurname";
-            labelSurname.Size = new Size(73, 20);
-            labelSurname.TabIndex = 0;
-            labelSurname.Text = "Фамилия";
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(16, 12);
+            listBox1.Name = "listBox1";
+            listBox1.SelectionMode = SelectionMode.None;
+            listBox1.Size = new Size(150, 484);
+            listBox1.TabIndex = 0;
             // 
-            // textBoxSurname
+            // label1
             // 
-            textBoxSurname.Location = new Point(34, 37);
-            textBoxSurname.Name = "textBoxSurname";
-            textBoxSurname.Size = new Size(335, 27);
-            textBoxSurname.TabIndex = 1;
-            textBoxSurname.TextChanged += textBoxSurname_TextChanged;
+            label1.AllowDrop = true;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Location = new Point(202, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 167);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
             // 
-            // dateTimePicker1
+            // textBox1
             // 
-            dateTimePicker1.Location = new Point(34, 196);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(335, 27);
-            dateTimePicker1.TabIndex = 2;
+            textBox1.Location = new Point(308, 514);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(262, 27);
+            textBox1.TabIndex = 2;
             // 
-            // comboBoxFlowers
+            // textBox2
             // 
-            comboBoxFlowers.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBoxFlowers.FormattingEnabled = true;
-            comboBoxFlowers.Items.AddRange(new object[] { "Розы", "Тюльпаны", "Лилии", "Ромашки", "Орхидеи" });
-            comboBoxFlowers.Location = new Point(34, 249);
-            comboBoxFlowers.Name = "comboBoxFlowers";
-            comboBoxFlowers.Size = new Size(335, 28);
-            comboBoxFlowers.TabIndex = 3;
-            comboBoxFlowers.SelectedIndexChanged += comboBoxFlowers_SelectedIndexChanged;
+            textBox2.Location = new Point(16, 514);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(253, 27);
+            textBox2.TabIndex = 3;
             // 
-            // panel1
+            // button1
             // 
-            panel1.Location = new Point(375, 280);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(64, 64);
-            panel1.TabIndex = 4;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(34, 173);
-            label2.Name = "label2";
-            label2.Size = new Size(107, 20);
-            label2.TabIndex = 5;
-            label2.Text = "Дата доставки";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(34, 226);
-            label3.Name = "label3";
-            label3.Size = new Size(53, 20);
-            label3.TabIndex = 6;
-            label3.Text = "Цветы";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(34, 280);
-            label4.Name = "label4";
-            label4.Size = new Size(110, 20);
-            label4.TabIndex = 7;
-            label4.Text = "Цвет упаковки";
-            // 
-            // comboBoxColors
-            // 
-            comboBoxColors.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBoxColors.FormattingEnabled = true;
-            comboBoxColors.Items.AddRange(new object[] { "Красный", "Синий", "Зелёный", "Жёлтый" });
-            comboBoxColors.Location = new Point(34, 303);
-            comboBoxColors.Name = "comboBoxColors";
-            comboBoxColors.Size = new Size(335, 28);
-            comboBoxColors.TabIndex = 8;
-            comboBoxColors.SelectedIndexChanged += comboBoxColors_SelectedIndexChanged;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(34, 334);
-            label5.Name = "label5";
-            label5.Size = new Size(142, 20);
-            label5.TabIndex = 9;
-            label5.Text = "Количество цветов";
-            // 
-            // textBoxFlowers
-            // 
-            textBoxFlowers.Location = new Point(34, 360);
-            textBoxFlowers.Name = "textBoxFlowers";
-            textBoxFlowers.Size = new Size(335, 27);
-            textBoxFlowers.TabIndex = 10;
-            textBoxFlowers.TextChanged += textBoxFlowers_TextChanged;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(34, 390);
-            label6.Name = "label6";
-            label6.Size = new Size(157, 20);
-            label6.TabIndex = 11;
-            label6.Text = "Надпись на открытке";
-            // 
-            // textBoxLetter
-            // 
-            textBoxLetter.Location = new Point(34, 413);
-            textBoxLetter.Name = "textBoxLetter";
-            textBoxLetter.Size = new Size(335, 27);
-            textBoxLetter.TabIndex = 12;
-            textBoxLetter.TextChanged += textBoxLetter_TextChanged;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(34, 491);
-            label7.Name = "label7";
-            label7.Size = new Size(112, 20);
-            label7.TabIndex = 13;
-            label7.Text = "Итоговая цена";
-            // 
-            // textBoxSummary
-            // 
-            textBoxSummary.Location = new Point(34, 514);
-            textBoxSummary.Name = "textBoxSummary";
-            textBoxSummary.ReadOnly = true;
-            textBoxSummary.Size = new Size(335, 27);
-            textBoxSummary.TabIndex = 14;
-            // 
-            // textBoxName
-            // 
-            textBoxName.Location = new Point(34, 90);
-            textBoxName.Name = "textBoxName";
-            textBoxName.Size = new Size(335, 27);
-            textBoxName.TabIndex = 16;
-            textBoxName.TextChanged += textBoxName_TextChanged;
-            // 
-            // labelName
-            // 
-            labelName.AutoSize = true;
-            labelName.Location = new Point(34, 67);
-            labelName.Name = "labelName";
-            labelName.Size = new Size(39, 20);
-            labelName.TabIndex = 15;
-            labelName.Text = "Имя";
-            // 
-            // textBoxFathname
-            // 
-            textBoxFathname.Location = new Point(34, 143);
-            textBoxFathname.Name = "textBoxFathname";
-            textBoxFathname.Size = new Size(335, 27);
-            textBoxFathname.TabIndex = 18;
-            textBoxFathname.TextChanged += textBoxFathname_TextChanged;
-            // 
-            // labelFathname
-            // 
-            labelFathname.AutoSize = true;
-            labelFathname.Location = new Point(34, 120);
-            labelFathname.Name = "labelFathname";
-            labelFathname.Size = new Size(72, 20);
-            labelFathname.TabIndex = 17;
-            labelFathname.Text = "Отчество";
-            // 
-            // labelErr1
-            // 
-            labelErr1.AutoSize = true;
-            labelErr1.Location = new Point(375, 40);
-            labelErr1.Name = "labelErr1";
-            labelErr1.Size = new Size(0, 20);
-            labelErr1.TabIndex = 19;
-            // 
-            // labelErr2
-            // 
-            labelErr2.AutoSize = true;
-            labelErr2.Location = new Point(375, 97);
-            labelErr2.Name = "labelErr2";
-            labelErr2.Size = new Size(0, 20);
-            labelErr2.TabIndex = 20;
-            // 
-            // labelErr3
-            // 
-            labelErr3.AutoSize = true;
-            labelErr3.Location = new Point(375, 150);
-            labelErr3.Name = "labelErr3";
-            labelErr3.Size = new Size(0, 20);
-            labelErr3.TabIndex = 21;
-            // 
-            // labelErr4
-            // 
-            labelErr4.AutoSize = true;
-            labelErr4.Location = new Point(375, 416);
-            labelErr4.Name = "labelErr4";
-            labelErr4.Size = new Size(0, 20);
-            labelErr4.TabIndex = 22;
+            button1.Location = new Point(429, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 98);
+            button1.TabIndex = 4;
+            button1.Text = "Генерировать новый массив";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
@@ -256,29 +86,11 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(582, 553);
-            Controls.Add(labelErr4);
-            Controls.Add(labelErr3);
-            Controls.Add(labelErr2);
-            Controls.Add(labelErr1);
-            Controls.Add(textBoxFathname);
-            Controls.Add(labelFathname);
-            Controls.Add(textBoxName);
-            Controls.Add(labelName);
-            Controls.Add(textBoxSummary);
-            Controls.Add(label7);
-            Controls.Add(textBoxLetter);
-            Controls.Add(label6);
-            Controls.Add(textBoxFlowers);
-            Controls.Add(label5);
-            Controls.Add(comboBoxColors);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(panel1);
-            Controls.Add(comboBoxFlowers);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(textBoxSurname);
-            Controls.Add(labelSurname);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Контрольная работа Вариант 7";
@@ -288,28 +100,10 @@
 
         #endregion
 
-        private Label labelSurname;
-        private TextBox textBoxSurname;
-        private DateTimePicker dateTimePicker1;
-        private ComboBox comboBoxFlowers;
-        private Panel panel1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private ComboBox comboBoxColors;
-        private Label label5;
-        private TextBox textBoxFlowers;
-        private Label label6;
-        private TextBox textBoxLetter;
-        private Label label7;
-        private TextBox textBoxSummary;
-        private TextBox textBoxName;
-        private Label labelName;
-        private TextBox textBoxFathname;
-        private Label labelFathname;
-        private Label labelErr1;
-        private Label labelErr2;
-        private Label labelErr3;
-        private Label labelErr4;
+        private ListBox listBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,149 +2,50 @@ using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Formats.Nrbf;
+using System.Security.Cryptography;
 using System.Windows.Forms;
-using Timer = System.Windows.Forms.Timer;
 
 namespace WinFormsAppEdu
 {
     public partial class Form1 : Form
     {
-
-        public double Price;
         public Form1()
         {
             InitializeComponent();
-            dateTimePicker1.MinDate = DateTime.Today;
-            dateTimePicker1.MaxDate = DateTime.Today.AddDays(31);
-
-            GraphicsPath path = new GraphicsPath();
-            path.AddEllipse(0, 0, panel1.Width, panel1.Height);
-            Region roundRegion = new Region(path);
-            panel1.Region = roundRegion;
-            panel1.BackColor = Color.White;
-        }
-        private void comboBoxColors_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            panel1.BackColor = GetColor(Convert.ToString(comboBoxColors.SelectedItem));
+            Load += button1_Click;
         }
 
-        private void comboBoxFlowers_SelectedIndexChanged(object sender, EventArgs e)
+        private void button1_Click(object? sender, EventArgs e)
         {
-            Price = GetPrice(Convert.ToString(comboBoxFlowers.SelectedItem));
-            UpdateSummaryTextBox();
-        }
-        private void textBoxFlowers_TextChanged(object sender, EventArgs e)
-        {
-            UpdateSummaryTextBox();
-        }
+            listBox1.Items.Clear();
+            label1.Text = "Массив: ";
 
-        private void textBoxSurname_TextChanged(object sender, EventArgs e)
-        {
-            if (!IsValidRussianString(textBoxSurname.Text) && textBoxSurname.Text != "")
+            Random rand = new Random();
+            int[] numbers = new int[20];
+            int maxElement = numbers[0];
+            int maxIndex = 0;
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
             {
-                labelErr1.Text = " - Некорректный ввод!!";
+                numbers[i] = rand.Next(-25, 31);
+                listBox1.Items.Add($"№{i + 1} = {numbers[i]}");
+                label1.Text += $"{numbers[i]}, ";
+
+                sum += numbers[i];
+
+                if (numbers[i] > maxElement)
+                {
+                    maxElement = numbers[i];
+                    maxIndex = i;
+                }
             }
-            else labelErr1.Text = "";
-        }
 
-        private void textBoxName_TextChanged(object sender, EventArgs e)
-        {
-            if (!IsValidRussianString(textBoxName.Text) && textBoxName.Text != "")
-            {
-                labelErr2.Text = " - Некорректный ввод!!";
-            }
-            else labelErr2.Text = "";
-        }
+            double average = sum / numbers.Length;
 
-        private void textBoxFathname_TextChanged(object sender, EventArgs e)
-        {
-            if (!IsValidRussianString(textBoxFathname.Text) && textBoxFathname.Text != "")
-            {
-                labelErr3.Text = " - Некорректный ввод!!";
-            }
-            else labelErr3.Text = "";
-        }
-
-        private void textBoxLetter_TextChanged(object sender, EventArgs e)
-        {
-            if (!IsValidRussianString(textBoxLetter.Text) && textBoxLetter.Text != "")
-            {
-                labelErr4.Text = " - Некорректный ввод!!";
-            }
-            else labelErr4.Text = "";
-        }
-
-        private void UpdateSummaryTextBox()
-        {
-            if (comboBoxFlowers.SelectedItem != null && textBoxFlowers.Text != "")
-            {
-                bool isValid = double.TryParse(textBoxFlowers.Text, out double quantity);
-                if (!isValid) textBoxSummary.Text = null;
-
-                textBoxSummary.Text = $"{(Price * quantity):C2}";
-            }
-            else
-            {
-                textBoxSummary.Text = null;
-            }
-        }
-        public Color GetColor(string? wrapping)
-        {
-            Color color = Color.White;
-            switch (wrapping)
-            {
-                case "Красный":
-                    color = Color.Red;
-                    break;
-                case "Синий":
-                    color = Color.Blue;
-                    break;
-                case "Зелёный":
-                    color = Color.Green;
-                    break;
-                case "Жёлтый":
-                    color = Color.Yellow;
-                    break;
-            }
-            return color;
-        }
-
-        public double GetPrice(string? flower)
-        {
-            double price = 0;
-            switch (flower)
-            {
-                case "Розы":
-                    price = 120;
-                    break;
-                case "Тюльпаны":
-                    price = 100;
-                    break;
-                case "Лилии":
-                    price = 140;
-                    break;
-                case "Ромашки":
-                    price = 80;
-                    break;
-                case "Орхидеи":
-                    price = 160;
-                    break;
-            }
-            return price;
-        }
-
-        public bool IsValidRussianString(string str)
-        {
-            if (string.IsNullOrEmpty(str))
-                return false;
-
-            char firstChar = str[0];
-            bool startsWithCapital = firstChar >= 'А' && firstChar <= 'Я';
-            if (firstChar == 'Ё')
-                startsWithCapital = true;
-
-            bool hasNoDigits = !str.Any(char.IsDigit);
-            return startsWithCapital && hasNoDigits;
+            textBox1.Text = $"Максимум: {maxElement} (№{maxIndex})";
+            textBox2.Text = $"Среднее арифметическое: {average:F2}";
+            label1.Text += $"{average}.";
         }
     }
 }
