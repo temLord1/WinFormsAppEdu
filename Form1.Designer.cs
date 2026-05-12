25586600b8e4d0357fab0b7e4755fdd7d0ef5646
Владимир Воронков <temlord34@gmail.com>
2026-05-12 08:37:40 +0500

    Задание 4.2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,65 +33,104 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             button1 = new Button();
+            label2 = new Label();
+            listBox2 = new ListBox();
+            label3 = new Label();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(16, 12);
+            listBox1.Location = new Point(14, 9);
+            listBox1.Margin = new Padding(3, 2, 3, 2);
             listBox1.Name = "listBox1";
-            listBox1.SelectionMode = SelectionMode.None;
-            listBox1.Size = new Size(150, 484);
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            listBox1.Size = new Size(132, 349);
             listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // label1
             // 
             label1.AllowDrop = true;
             label1.FlatStyle = FlatStyle.Flat;
-            label1.Location = new Point(202, 12);
+            label1.Location = new Point(375, 92);
             label1.Name = "label1";
-            label1.Size = new Size(192, 167);
+            label1.Size = new Size(123, 125);
             label1.TabIndex = 1;
             label1.Text = "label1";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(308, 514);
+            textBox1.Location = new Point(270, 386);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
-            textBox1.Size = new Size(262, 27);
+            textBox1.Size = new Size(230, 23);
             textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(16, 514);
+            textBox2.Location = new Point(14, 386);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.ReadOnly = true;
-            textBox2.Size = new Size(253, 27);
+            textBox2.Size = new Size(222, 23);
             textBox2.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Location = new Point(429, 12);
+            button1.Location = new Point(375, 9);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(141, 98);
+            button1.Size = new Size(123, 74);
             button1.TabIndex = 4;
-            button1.Text = "Генерировать новый массив";
+            button1.Text = "Генерировать новые массивы";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label2
+            // 
+            label2.AllowDrop = true;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Location = new Point(375, 230);
+            label2.Name = "label2";
+            label2.Size = new Size(123, 125);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(178, 9);
+            listBox2.Margin = new Padding(3, 2, 3, 2);
+            listBox2.Name = "listBox2";
+            listBox2.SelectionMode = SelectionMode.MultiSimple;
+            listBox2.Size = new Size(132, 349);
+            listBox2.TabIndex = 6;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AllowDrop = true;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Location = new Point(14, 363);
+            label3.Name = "label3";
+            label3.Size = new Size(187, 20);
+            label3.TabIndex = 7;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(582, 553);
+            ClientSize = new Size(509, 415);
+            Controls.Add(label3);
+            Controls.Add(listBox2);
+            Controls.Add(label2);
             Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(listBox1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Контрольная работа Вариант 7";
             ResumeLayout(false);
@@ -105,5 +144,8 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Button button1;
+        private Label label2;
+        private ListBox listBox2;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -9,43 +9,100 @@ namespace WinFormsAppEdu
 {
     public partial class Form1 : Form
     {
+        public bool isProgrammaticChange = false;
+
         public Form1()
         {
             InitializeComponent();
             Load += button1_Click;
+            textBox2.TextChanged += FindElement;
+            button1.Click += FindElement;
         }
 
         private void button1_Click(object? sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            label1.Text = "Массив: ";
+            listBox2.Items.Clear();
+            label1.Text = "Массив 1: ";
+            label2.Text = "Массив 2: ";
 
             Random rand = new Random();
-            int[] numbers = new int[20];
-            int maxElement = numbers[0];
-            int maxIndex = 0;
-            double sum = 0;
+            int[] numbers1 = new int[20];
+            int[] numbers2 = new int[25];
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < numbers1.Length; i++)
             {
-                numbers[i] = rand.Next(-25, 31);
-                listBox1.Items.Add($"№{i + 1} = {numbers[i]}");
-                label1.Text += $"{numbers[i]}, ";
+                numbers1[i] = rand.Next(-25, 31);
+                listBox1.Items.Add($"№{i + 1} = {numbers1[i]}");
+                label1.Text += $"{numbers1[i]}, ";
+            }
 
-                sum += numbers[i];
+            for (int i = 0; i < numbers2.Length; i++)
+            {
+                numbers2[i] = rand.Next(-25, 31);
+                listBox2.Items.Add($"№{i + 1} = {numbers2[i]}");
+                label2.Text += $"{numbers2[i]}, ";
+            }
+        }
 
-                if (numbers[i] > maxElement)
+        private void FindElement(object? sender, EventArgs e)
+        {
+            isProgrammaticChange = true;
+
+            listBox1.ClearSelected();
+            listBox2.ClearSelected();
+
+            if (!int.TryParse(textBox2.Text, out int number))
+            {
+                label3.Text = "Некорректный ввод!";
+                if (textBox2.Text == "")
                 {
-                    maxElement = numbers[i];
-                    maxIndex = i;
+                    label3.Text = "";
+                }
+                isProgrammaticChange = false;
+                return;
+            }
+
+            label3.Text = "";
+            string searchPattern = "= " + number.ToString();
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string itemText = listBox1.Items[i].ToString();
+
+                if (itemText.EndsWith(searchPattern))
+                {
+                    listBox1.SelectedIndex = i;
                 }
             }
 
-            double average = sum / numbers.Length;
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                string itemText = listBox2.Items[i].ToString();
 
-            textBox1.Text = $"Максимум: {maxElement} (№{maxIndex})";
-            textBox2.Text = $"Среднее арифметическое: {average:F2}";
-            label1.Text += $"{average}.";
+                if (itemText.EndsWith(searchPattern))
+                {
+                    listBox2.SelectedIndex = i;
+                }
+            }
+
+            isProgrammaticChange = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!isProgrammaticChange)
+            {
+                FindElement(sender, e);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!isProgrammaticChange)
+            {
+                FindElement(sender, e);
+            }
         }
     }
 }
