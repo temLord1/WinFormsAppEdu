d643b8f3d3adee8e6658e4fd12ae47b3ac6f470e
Владимир Воронков <temlord34@gmail.com>
2026-05-26 03:49:36 +0500

    Задание 4.3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,124 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            dgv1 = new DataGridView();
+            panel1 = new Panel();
+            tb3 = new TextBox();
+            tb2 = new TextBox();
+            tb1 = new TextBox();
             label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            button1 = new Button();
-            label2 = new Label();
-            listBox2 = new ListBox();
-            label3 = new Label();
+            cmb1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dgv1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // listBox1
+            // dgv1
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(14, 9);
-            listBox1.Margin = new Padding(3, 2, 3, 2);
-            listBox1.Name = "listBox1";
-            listBox1.SelectionMode = SelectionMode.MultiSimple;
-            listBox1.Size = new Size(132, 349);
-            listBox1.TabIndex = 0;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            dgv1.AllowUserToAddRows = false;
+            dgv1.AllowUserToDeleteRows = false;
+            dgv1.AllowUserToResizeColumns = false;
+            dgv1.AllowUserToResizeRows = false;
+            dgv1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv1.BorderStyle = BorderStyle.None;
+            dgv1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 8F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dgv1.DefaultCellStyle = dataGridViewCellStyle1;
+            dgv1.Dock = DockStyle.Fill;
+            dgv1.Location = new Point(0, 0);
+            dgv1.Name = "dgv1";
+            dgv1.ReadOnly = true;
+            dgv1.RowHeadersVisible = false;
+            dgv1.ScrollBars = ScrollBars.None;
+            dgv1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dgv1.Size = new Size(580, 357);
+            dgv1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(tb3);
+            panel1.Controls.Add(tb2);
+            panel1.Controls.Add(tb1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(cmb1);
+            panel1.Location = new Point(0, 256);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(331, 101);
+            panel1.TabIndex = 1;
+            panel1.MouseDown += panel1_MouseDown;
+            panel1.MouseMove += panel1_MouseMove;
+            panel1.MouseUp += panel1_MouseUp;
+            // 
+            // tb3
+            // 
+            tb3.Location = new Point(145, 66);
+            tb3.Name = "tb3";
+            tb3.ReadOnly = true;
+            tb3.Size = new Size(167, 23);
+            tb3.TabIndex = 4;
+            // 
+            // tb2
+            // 
+            tb2.Location = new Point(145, 37);
+            tb2.Name = "tb2";
+            tb2.ReadOnly = true;
+            tb2.Size = new Size(167, 23);
+            tb2.TabIndex = 3;
+            // 
+            // tb1
+            // 
+            tb1.Location = new Point(145, 8);
+            tb1.Name = "tb1";
+            tb1.ReadOnly = true;
+            tb1.Size = new Size(167, 23);
+            tb1.TabIndex = 2;
             // 
             // label1
             // 
-            label1.AllowDrop = true;
-            label1.FlatStyle = FlatStyle.Flat;
-            label1.Location = new Point(375, 92);
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 8);
             label1.Name = "label1";
-            label1.Size = new Size(123, 125);
+            label1.Size = new Size(92, 15);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Выбор столбца";
             // 
-            // textBox1
+            // cmb1
             // 
-            textBox1.Location = new Point(270, 386);
-            textBox1.Margin = new Padding(3, 2, 3, 2);
-            textBox1.Name = "textBox1";
-            textBox1.ReadOnly = true;
-            textBox1.Size = new Size(230, 23);
-            textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(14, 386);
-            textBox2.Margin = new Padding(3, 2, 3, 2);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(222, 23);
-            textBox2.TabIndex = 3;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(375, 9);
-            button1.Margin = new Padding(3, 2, 3, 2);
-            button1.Name = "button1";
-            button1.Size = new Size(123, 74);
-            button1.TabIndex = 4;
-            button1.Text = "Генерировать новые массивы";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // label2
-            // 
-            label2.AllowDrop = true;
-            label2.FlatStyle = FlatStyle.Flat;
-            label2.Location = new Point(375, 230);
-            label2.Name = "label2";
-            label2.Size = new Size(123, 125);
-            label2.TabIndex = 5;
-            label2.Text = "label2";
-            // 
-            // listBox2
-            // 
-            listBox2.FormattingEnabled = true;
-            listBox2.Location = new Point(178, 9);
-            listBox2.Margin = new Padding(3, 2, 3, 2);
-            listBox2.Name = "listBox2";
-            listBox2.SelectionMode = SelectionMode.MultiSimple;
-            listBox2.Size = new Size(132, 349);
-            listBox2.TabIndex = 6;
-            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
-            // 
-            // label3
-            // 
-            label3.AllowDrop = true;
-            label3.FlatStyle = FlatStyle.Flat;
-            label3.Location = new Point(14, 363);
-            label3.Name = "label3";
-            label3.Size = new Size(187, 20);
-            label3.TabIndex = 7;
+            cmb1.FormattingEnabled = true;
+            cmb1.Location = new Point(8, 26);
+            cmb1.Name = "cmb1";
+            cmb1.Size = new Size(121, 23);
+            cmb1.TabIndex = 0;
+            cmb1.SelectedIndexChanged += cmb1_SelectedIndexChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(509, 415);
-            Controls.Add(label3);
-            Controls.Add(listBox2);
-            Controls.Add(label2);
-            Controls.Add(button1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
-            Controls.Add(listBox1);
+            ClientSize = new Size(580, 357);
+            Controls.Add(panel1);
+            Controls.Add(dgv1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            KeyPreview = true;
             Name = "Form1";
-            Text = "Контрольная работа Вариант 7";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "I JUST MADE SOME BULLSHI";
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)dgv1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private ListBox listBox1;
+        private DataGridView dgv1;
+        private Panel panel1;
         private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Button button1;
-        private Label label2;
-        private ListBox listBox2;
-        private Label label3;
+        private ComboBox cmb1;
+        private TextBox tb3;
+        private TextBox tb2;
+        private TextBox tb1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,105 +3,203 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Formats.Nrbf;
 using System.Security.Cryptography;
+using System.Transactions;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace WinFormsAppEdu
 {
     public partial class Form1 : Form
     {
-        public bool isProgrammaticChange = false;
+        public double[,] matrix;
+        public int x = 1;
+        public int y = 1;
+
+        public bool isDragging = false;
+        public Point startPoint = new Point(0, 0);
 
         public Form1()
         {
             InitializeComponent();
-            Load += button1_Click;
-            textBox2.TextChanged += FindElement;
-            button1.Click += FindElement;
+            InitMatrix(10, 10);
+            AskForParameters();
         }
 
-        private void button1_Click(object? sender, EventArgs e)
+        private void InitMatrix(int rows, int columns)
         {
-            listBox1.Items.Clear();
-            listBox2.Items.Clear();
-            label1.Text = "Массив 1: ";
-            label2.Text = "Массив 2: ";
+            matrix = new double[rows, columns];
 
-            Random rand = new Random();
-            int[] numbers1 = new int[20];
-            int[] numbers2 = new int[25];
-
-            for (int i = 0; i < numbers1.Length; i++)
+            for (int i = 0; i < rows; i++)
             {
-                numbers1[i] = rand.Next(-25, 31);
-                listBox1.Items.Add($"№{i + 1} = {numbers1[i]}");
-                label1.Text += $"{numbers1[i]}, ";
-            }
-
-            for (int i = 0; i < numbers2.Length; i++)
-            {
-                numbers2[i] = rand.Next(-25, 31);
-                listBox2.Items.Add($"№{i + 1} = {numbers2[i]}");
-                label2.Text += $"{numbers2[i]}, ";
-            }
-        }
-
-        private void FindElement(object? sender, EventArgs e)
-        {
-            isProgrammaticChange = true;
-
-            listBox1.ClearSelected();
-            listBox2.ClearSelected();
-
-            if (!int.TryParse(textBox2.Text, out int number))
-            {
-                label3.Text = "Некорректный ввод!";
-                if (textBox2.Text == "")
+                for (int j = 0; j < columns; j++)
                 {
-                    label3.Text = "";
-                }
-                isProgrammaticChange = false;
-                return;
-            }
 
-            label3.Text = "";
-            string searchPattern = "= " + number.ToString();
-
-            for (int i = 0; i < listBox1.Items.Count; i++)
-            {
-                string itemText = listBox1.Items[i].ToString();
-
-                if (itemText.EndsWith(searchPattern))
-                {
-                    listBox1.SelectedIndex = i;
+                    int i_math = i + 1;
+                    int j_math = j + 1;
+                    if (i_math > j_math)
+                    {
+                        matrix[i, j] = ((x + y) * i_math) / ((2 * x + y * Math.Exp(x)) * x);
+                    }
+                    else if (j_math == i_math)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    else
+                    {
+                        matrix[i, j] = Math.Exp(i_math * x + y) / (y * i_math);
+                    }
                 }
             }
 
-            for (int i = 0; i < listBox2.Items.Count; i++)
-            {
-                string itemText = listBox2.Items[i].ToString();
+            DisplayMatrix();
+        }
 
-                if (itemText.EndsWith(searchPattern))
+        private void DisplayMatrix()
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            dgv1.Columns.Clear();
+            dgv1.Rows.Clear();
+            cmb1.Items.Clear();
+
+            for (int j = 0; j < cols; j++)
+            {
+                dgv1.Columns.Add($"col{j}", "");
+                cmb1.Items.Add($"Столбец {j + 1}");
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                dgv1.Rows.Add();
+                for (int j = 0; j < cols; j++)
                 {
-                    listBox2.SelectedIndex = i;
+                    dgv1.Rows[i].Cells[j].Value = matrix[i, j].ToString("G2");
                 }
             }
 
-            isProgrammaticChange = false;
+            if (cmb1.Items.Count > 0)
+            {
+                cmb1.SelectedIndex = 0;
+            }
+
+            AdjustMatrix();
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void AskForParameters()
         {
-            if (!isProgrammaticChange)
+            using (Form2 inputForm = new Form2())
             {
-                FindElement(sender, e);
+                if (inputForm.ShowDialog() == DialogResult.OK)
+                {
+                    x = inputForm.X;
+                    y = inputForm.Y;
+
+                    InitMatrix(10, 10);
+                }
             }
         }
 
-        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void AdjustMatrix()
         {
-            if (!isProgrammaticChange)
+            if (dgv1.RowCount == 0) return;
+
+            int avaibleHeight = dgv1.ClientSize.Height;
+            int rowHeight = avaibleHeight / dgv1.RowCount;
+
+            if (rowHeight < 25) rowHeight = 25;
+
+            foreach (DataGridViewRow row in dgv1.Rows)
             {
-                FindElement(sender, e);
+                row.Height = rowHeight;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+            {
+                dgv1.ClearSelection();
+                AskForParameters();
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                startPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point p = PointToClient(Cursor.Position);
+                panel1.Left = p.X - startPoint.X;
+                panel1.Top = p.Y - startPoint.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
+        }
+
+        private void cmb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmb1.SelectedIndex < 0 || matrix == null) return;
+
+            int sCol = cmb1.SelectedIndex;
+            int rowsCount = matrix.GetLength(0);
+            double sum = 0;
+            double maxVal = double.MinValue;
+            double firstMin = double.MaxValue;
+            double secondMin = double.MaxValue;
+            double avgVal = 0;
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                double current = matrix[i, sCol];
+
+                sum += current;
+
+                if (current > maxVal)
+                {
+                    maxVal = current;
+                }
+
+                if (current < firstMin)
+                {
+                    secondMin = firstMin;
+                    firstMin = current;
+                }
+                else if (current < secondMin && current != firstMin)
+                {
+                    secondMin = current;
+                }
+            }
+
+            avgVal = sum / rowsCount;
+
+            tb1.Text = $"Среднее: {avgVal:G6}";
+            tb2.Text = $"Максимум: {maxVal:G6}";
+            bool isExist = secondMin == double.MaxValue;
+            tb3.Text = isExist ? "2-й минимум: нет" : $"2-й минимум: {secondMin:G6}";
+
+            if (dgv1.ColumnCount > 0)
+            {
+                dgv1.ClearSelection();
+                foreach (DataGridViewRow row in dgv1.Rows)
+                {
+                    row.Cells[sCol].Selected = true;
+                }
             }
         }
     }
